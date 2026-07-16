b173434bfcde0a10472de139b69908adb3eb7225
AndresFelipeR <feliperoher@gmail.com>
2026-07-16 23:49:23 +0200

    chore: update gitignore

---
+++ src/Bookly.Domain/ValueObjects/Duracion.cs
@@ -0,0 +1,22 @@
+﻿using Blookly.Domain.Common;
+
+namespace Blookly.Domain.ValueObjects;
+
+public sealed class Duracion : ValueObject
+{
+
+    public int Minutos { get; private set; }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Minutos;
+    }
+
+    public Duracion(int minutos)
+    {
+        if (minutos <= 0)
+            throw new ArgumentException("La duración debe ser mayor a cero.", nameof(minutos));
+
+        Minutos = minutos;
+    }
+}
---
+++ src/Bookly.Domain/ValueObjects/Money.cs
@@ -0,0 +1,28 @@
+﻿using Blookly.Domain.Common;
+
+namespace Blookly.Domain.ValueObjects;
+
+public sealed class Money : ValueObject
+{
+    public decimal Amount { get; private set; }
+    public string Currency { get; private set; }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Amount;
+        yield return Currency;
+    }
+
+    public Money(decimal amount, string currency)
+    {
+        if (amount < 0)
+            throw new ArgumentException("El monto no puede ser negativo.", nameof(amount));
+
+        if (string.IsNullOrWhiteSpace(currency))
+            throw new ArgumentException("La moneda no puede estar vacía.", nameof(currency));
+
+        Amount = amount;
+        Currency = currency;
+    }
+
+}

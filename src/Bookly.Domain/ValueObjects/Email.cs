b3ca354b388496f71d58c2a691e4ed2a3afc10e0
AndresFelipeR <feliperoher@gmail.com>
2026-07-16 20:13:19 +0200

    se añade ValuesObject Email y PhoneNumber

--- src/Bookly.Domain/ValueObjects/Email.cs
+++ src/Bookly.Domain/ValueObjects/Email.cs
@@ -1,10 +1,16 @@
 ﻿using System.Net.Mail;
+using Blookly.Domain.Common;
 
 namespace Blookly.Domain.ValueObjects;
 
-public sealed class Email
+public sealed class Email : ValueObject
 {
     public string Value { get; }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+    }
     public Email(string value)
     {
         if(string.IsNullOrWhiteSpace(value))
---
+++ src/Bookly.Domain/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,21 @@
+﻿using Blookly.Domain.Common;
+
+namespace Blookly.Domain.ValueObjects;
+
+public sealed class PhoneNumber : ValueObject
+{
+    public string Value { get; }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+    public PhoneNumber(string value)
+    {
+        if(string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("El numero de telefono no puede estar vacio");
+
+        Value = value;
+    }
+}

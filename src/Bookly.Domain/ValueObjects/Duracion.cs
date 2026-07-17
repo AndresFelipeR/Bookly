f8e75ba8d33c0bb8ae2afecc026ba5e3c928d8dd
AndresFelipeR <feliperoher@gmail.com>
2026-07-17 18:50:07 +0200

    feat(dominio): Mejora a mi ValueObject(Duracion)

--- src/Bookly.Domain/ValueObjects/Duracion.cs
+++ src/Bookly.Domain/ValueObjects/Duracion.cs
@@ -5,18 +5,49 @@ namespace Blookly.Domain.ValueObjects;
 public sealed class Duracion : ValueObject
 {
 
-    public int Minutos { get; private set; }
-
-    protected override IEnumerable<object?> GetEqualityComponents()
+    public TimeSpan Value { get;}
+    public static Duracion FromMinutes(int minutes)
     {
-        yield return Minutos;
+        return new Duracion(TimeSpan.FromMinutes(minutes));
+    }
+    public static Duracion FromHours(int hours)
+    {
+        return new Duracion(TimeSpan.FromHours(hours));
     }
 
-    public Duracion(int minutos)
+    public static Duracion FromTimeSpan(TimeSpan timeSpan)
     {
-        if (minutos <= 0)
-            throw new ArgumentException("La duración debe ser mayor a cero.", nameof(minutos));
+       return new Duracion(timeSpan);
+    }
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+    }
 
-        Minutos = minutos;
+    private Duracion(TimeSpan value)
+    {
+        if (value <= TimeSpan.Zero)
+            throw new ArgumentException("La duración debe ser mayor a cero.", nameof(value));
+
+        Value = value;
+    }
+
+    public int TotalMinutes => (int)Value.TotalMinutes;
+    public double TotalHours => Value.TotalHours;
+
+    public Duracion Sumar(Duracion other)
+    {
+        ArgumentNullException.ThrowIfNull(other);
+        return new Duracion(Value + other.Value);
+    }
+
+    public Duracion Restar(Duracion other)
+    {
+        ArgumentNullException.ThrowIfNull(other);
+        var result = Value - other.Value;
+        if (result <= TimeSpan.Zero)
+            throw new InvalidOperationException("La duración resultante no puede ser menor o igual a cero.");
+
+        return new Duracion(result);
     }
 }

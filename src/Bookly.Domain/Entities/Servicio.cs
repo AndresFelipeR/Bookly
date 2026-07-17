d056d54e1b830cb9b684f80c382073324c8df54a
AndresFelipeR <feliperoher@gmail.com>
2026-07-18 00:07:52 +0200

    feat(dominio) Clase de Servicios

--- src/Bookly.Domain/Entities/Servicio.cs
+++ src/Bookly.Domain/Entities/Servicio.cs
@@ -12,14 +12,16 @@ public sealed class Servicio : BaseEntity
     public TipoServicio TipoServicio { get; private set; }
     public Duracion Duracion { get; private set; }
     public Guid TipoServicioId { get; private set; } // esto es para EF Core, para que pueda mapear la relación con TipoServicio
+    public PoliticaReserva PoliticaReserva { get; private set; }
 
-    private Servicio(string nombre, string descripcion, Money precio, TipoServicio tipoServicio, Duracion duracion)
+    private Servicio(string nombre, string descripcion, Money precio, TipoServicio tipoServicio, Duracion duracion, PoliticaReserva politicaReserva)
     {
         ArgumentNullException.ThrowIfNull(nombre);
         ArgumentNullException.ThrowIfNull(descripcion);
         ArgumentNullException.ThrowIfNull(tipoServicio);
         ArgumentNullException.ThrowIfNull(precio);
         ArgumentNullException.ThrowIfNull(duracion);
+        ArgumentNullException.ThrowIfNull(politicaReserva);
 
         Nombre = nombre;
         Descripcion = descripcion;
@@ -27,6 +29,7 @@ public sealed class Servicio : BaseEntity
         TipoServicio = tipoServicio;
         Duracion = duracion;
         TipoServicioId = tipoServicio.Id;
+        PoliticaReserva = politicaReserva;
     }
 
     protected Servicio()
@@ -34,9 +37,79 @@ public sealed class Servicio : BaseEntity
         // Constructor protegido para EF Core
     }
 
-    public static Servicio Create(string nombre, string descripcion, Money precio, TipoServicio tipoServicio, Duracion duracion)
+    public static Servicio Create(string nombre, string descripcion, Money precio, TipoServicio tipoServicio, Duracion duracion, PoliticaReserva politicaReserva)
     {
-        return new Servicio(nombre, descripcion, precio, tipoServicio, duracion);
+        return new Servicio(nombre, descripcion, precio, tipoServicio, duracion, politicaReserva);
     }
-    
+
+    public void CambiarDescripcion(string nuevaDescripcion)
+    {
+        if (string.IsNullOrWhiteSpace(nuevaDescripcion))
+            throw new ArgumentException("La descripción no puede estar vacía.", nameof(nuevaDescripcion));
+        if(nuevaDescripcion == Descripcion)
+            return; // No hacer nada si la descripción es la misma
+        Descripcion = nuevaDescripcion;
+    }
+
+public void CambiarNombre(string nuevoNombre)
+    {
+        if (string.IsNullOrWhiteSpace(nuevoNombre))
+            throw new ArgumentException("El nombre no puede estar vacío.", nameof(nuevoNombre));
+        if(nuevoNombre == Nombre)
+            return; // No hacer nada si el nombre es el mismo
+        Nombre = nuevoNombre;
+    }
+    public void CambiarPrecio(Money nuevoPrecio)
+    {
+        ArgumentNullException.ThrowIfNull(nuevoPrecio);
+        if(nuevoPrecio == Precio)
+            return; // No hacer nada si el precio es el mismo
+        Precio = nuevoPrecio;
+    }
+
+    public void CambiarDuracion(Duracion nuevaDuracion)
+    {
+        ArgumentNullException.ThrowIfNull(nuevaDuracion);
+        if(nuevaDuracion == Duracion)
+            return; // No hacer nada si la duración es la misma
+        Duracion = nuevaDuracion;
+    }
+
+    public void Activar()
+    {
+        if (State)
+            throw new InvalidOperationException("El servicio ya está activo.");
+        State = true;
+    }
+
+    public void Desactivar()
+    {
+        if (!State)
+            throw new InvalidOperationException("El servicio ya está inactivo.");
+        State = false;
+    }
+
+    public void CambiarTipoServicio(TipoServicio nuevoTipoServicio)
+    {
+        ArgumentNullException.ThrowIfNull(nuevoTipoServicio);
+
+        if(nuevoTipoServicio.Id == TipoServicioId)
+            throw new InvalidOperationException("El servicio ya pertenece a este tipo de servicio.");
+        
+        if(!nuevoTipoServicio.State)
+            throw new InvalidOperationException("No se puede asignar un tipo de servicio inactivo.");
+        
+
+        TipoServicio = nuevoTipoServicio;
+        TipoServicioId = nuevoTipoServicio.Id;
+    }
+
+    public void CambiarPoliticaReserva(PoliticaReserva nuevaPoliticaReserva)
+    {
+        ArgumentNullException.ThrowIfNull(nuevaPoliticaReserva);
+        if(nuevaPoliticaReserva == PoliticaReserva)
+            return; // No hacer nada si la política de reserva es la misma
+        PoliticaReserva = nuevaPoliticaReserva;
+    }
+   
 }

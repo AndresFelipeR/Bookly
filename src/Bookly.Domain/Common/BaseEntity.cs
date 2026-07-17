26823152c221f13855f71657e02d50ed0754a0f1
AndresFelipeR <feliperoher@gmail.com>
2026-07-17 17:55:18 +0200

    agregando state en BaseEntity

--- src/Bookly.Domain/Common/BaseEntity.cs
+++ src/Bookly.Domain/Common/BaseEntity.cs
@@ -5,4 +5,5 @@ public abstract class BaseEntity
     public Guid Id { get; protected set; }
     public DateTime CreatedAt { get; protected set; }
     public DateTime? UpdatedAt { get; protected set; }
+    public bool State { get; protected set; } = true;
 }

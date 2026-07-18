1d34a8e59c770fc1cd5685c413b282c96f7f5311
AndresFelipeR <feliperoher@gmail.com>
2026-07-18 09:05:14 +0200

    feat(Domain): BaseEntity

--- src/Bookly.Domain/Common/BaseEntity.cs
+++ src/Bookly.Domain/Common/BaseEntity.cs
@@ -6,4 +6,19 @@ public abstract class BaseEntity
     public DateTime CreatedAt { get; protected set; }
     public DateTime? UpdatedAt { get; protected set; }
     public bool State { get; protected set; } = true;
+
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        ArgumentNullException.ThrowIfNull(domainEvent);
+        _domainEvents.Add(domainEvent);
+        
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
 }
